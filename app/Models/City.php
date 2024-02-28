87df5987909d0f8c4df37d44483239f1047323f8
Dimitrij Jedich <dimitrij@startup-werk.de>
2024-02-28 16:46:23 +0100

    add PHPDoc properties from the database for models country, state and city

--- app/Models/City.php
+++ app/Models/City.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $state_id
+ * @property string $name
+ */
 class City extends Model
 {
     use HasFactory;
