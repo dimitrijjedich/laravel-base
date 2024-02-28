ee08ad5fd42a461d809cf918ed6cdb1dd5fd632a
Dimitrij Jedich <dimitrij@startup-werk.de>
2024-02-28 16:45:00 +0100

    extend DatabaseSeeder.php by countries, states and cities

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
+use App\Models\Country;
+use App\Models\State;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,5 +14,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $usa = new Country;
+        $usa->name = 'USA';
+        $usa->save();
+        $newYork = new State;
+        $newYork->name = 'New York';
+        $newYork->country_id = $usa->id;
+        $newYork->save();
+        $washington = new State;
+        $washington->name = 'Washington';
+        $washington->country_id = $usa->id;
+        $washington->save();
+
+        $germany = new Country;
+        $germany->name = 'Germany';
+        $germany->save();
+        $berlin = new State;
+        $berlin->name = 'Berlin';
+        $berlin->country_id = $germany->id;
+        $berlin->save();
+        $bremen = new State;
+        $bremen->name = 'Bremen';
+        $bremen->country_id = $germany->id;
+        $bremen->save();
+
+        $albany = new City();
+        $albany->name = 'Albany';
+        $albany->state_id = $newYork->id;
+        $albany->save();
+        $seattle = new City();
+        $seattle->name = 'Seattle';
+        $seattle->state_id = $washington->id;
+        $seattle->save();
+        $olympia = new City();
+        $olympia->name = 'Olympia';
+        $olympia->state_id = $washington->id;
+        $olympia->save();
+
+        $berlinCity = new City;
+        $berlinCity->name = 'Berlin';
+        $berlinCity->state_id = $berlin->id;
+        $berlinCity->save();
+        $bremenCity = new City();
+        $bremenCity->name = 'Bremen';
+        $bremenCity->state_id = $bremen->id;
+        $bremenCity->save();
     }
 }
