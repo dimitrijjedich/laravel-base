a795373e205841eddac13df0a39c0259fad27b89
dimitrijjedich <dimitrij@startup-werk.de>
2024-03-11 14:11:11 +0100

    add scheduled command to project

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            info("Call happened at". now());
+            (new DatabaseSeeder())->run();
+        })->everyMinute();
     }
 
     /**
