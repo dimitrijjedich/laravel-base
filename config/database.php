113869cc6e79feb1928dc98920e448587719ccd4
dimitrijjedich <dimitrij@startup-werk.de>
2024-03-19 14:42:41 +0100

    fix typo

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB__PGSQL_HOST', '127.0.0.1'),
+            'host' => env('DB_PGSQL_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
